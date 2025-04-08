1ccac9955eb9c9f27cc25a4efdd89097c3c04b93
judiciary-colombia-ddpd <gpoveda@deaj.ramajudicial.gov.co>
2025-04-08 08:59:08 -0500

    microsoft scopes

--- src/utils/getMicrosoftUrl.ts
+++ src/utils/getMicrosoftUrl.ts
@@ -8,8 +8,7 @@ export const getGoogleUrl = (from: string) => {
     response_type: "code",
     prompt: "consent",
     scope: [
-      "https://www.googleapis.com/auth/userinfo.profile",
-      "https://www.googleapis.com/auth/userinfo.email",
+      
     ].join(" "),
     state: from,
   };
