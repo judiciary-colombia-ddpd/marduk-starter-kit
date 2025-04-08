bba322f1e2c12b12e1a304856ba912bc51a6bc9e
judiciary-colombia-ddpd <gpoveda@deaj.ramajudicial.gov.co>
2025-04-08 08:58:43 -0500

    microsoft ouath

--- src/utils/getMicrosoftUrl.ts
+++ src/utils/getMicrosoftUrl.ts
@@ -1,5 +1,5 @@
 export const getGoogleUrl = (from: string) => {
-  const rootUrl = `https://accounts.google.com/o/oauth2/v2/auth`;
+  const rootUrl = `https://microsoft.com/o/oauth2/v2/auth`;
 
   const options = {
     redirect_uri: import.meta.env.VITE_GOOGLE_OAUTH_REDIRECT as string,
