b86c857b6b31cc9b475b641b01bec282893ab029
judiciary-colombia-ddpd <gpoveda@deaj.ramajudicial.gov.co>
2025-04-08 09:08:02 -0500

    disable external oauth

--- src/pages/login.page.tsx
+++ src/pages/login.page.tsx
@@ -160,38 +160,6 @@ const LoginPage = () => {
               <p className="text-center font-semibold mx-4 mb-0">OR</p>
             </div>
 
-            <a
-              className="px-7 py-2 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
-              style={{ backgroundColor: "#3b5998" }}
-              href={getGoogleUrl(from)}
-              role="button"
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-            >
-              <img
-                className="pr-2"
-                src={GoogleLogo}
-                alt=""
-                style={{ height: "2rem" }}
-              />
-              Continue with Google
-            </a>
-            <a
-              className="px-7 py-2 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center"
-              style={{ backgroundColor: "#55acee" }}
-              href={getGitHubUrl(from)}
-              role="button"
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-            >
-              <img
-                className="pr-2"
-                src={GitHubLogo}
-                alt=""
-                style={{ height: "2.2rem" }}
-              />
-              Continue with GitHub
-            </a>
           </form>
         </div>
       </div>
