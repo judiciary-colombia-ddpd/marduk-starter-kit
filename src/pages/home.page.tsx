8c477da67ce5a57293ddc6d8e66a75172661847b
DDPD Judiciary of Colombia <gpoveda@deaj.ramajudicial.gov.co>
2026-01-02 17:19:27 +0000

    chaning bg-style

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,10 +1,10 @@
 const HomePage = () => {
   return (
     <>
-      <section className="bg-ct-blue-600 min-h-screen pt-20">
+      <section className="min-h-screen pt-20" style="background-color: #879CD6;">
         <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
           <p className="text-3xl font-semibold">
-            Welcome to Google OAuth2 with React.js
+            Welcome to Rama Judicial Oauth with React.js
           </p>
         </div>
       </section>
