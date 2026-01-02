f3bc1a557b32634a0f2e4d177ed8c067a9c291ea
DDPD Judiciary of Colombia <gpoveda@deaj.ramajudicial.gov.co>
2026-01-02 18:45:35 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -47,7 +47,7 @@ const Header = () => {
         <nav className="h-full flex justify-between container items-center">
           <div>
             <Link to="/" className="text-ct-dark-600 text-2xl font-semibold">
-              CodevoWeb
+              SIRNA - TEST
             </Link>
           </div>
           <ul className="flex items-center gap-4">
